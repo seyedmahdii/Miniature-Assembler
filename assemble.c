1da2f3f87f783b0e96e0d963f39064b0766938ca
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2022-03-30 00:24:40 +0430

    .fill directive

--- assemble.c
+++ assemble.c
@@ -1,14 +1,20 @@
 #include "assemble.h"
+const int memorySize = 8192;
 
 void main(int argc,char **argv){
    FILE *assp, *machp, *fopen();
    struct symbolTable *pSymbolTable;
    int symbolTableLen;
+   int registers[16];
+   registers[0] = 0;
+   struct memoryTable *pMemoryTable;
+   int memoryCounter = 0;
    int i, j, found, noInsts;
    struct instruction *currInst;
    size_t lineSize;
    char *line;
    char *token;
+   char lable[7];
    char *instructions[] = {
       "add", "sub", "slt", "or", "nand",
       "addi", "slti", "ori", "lui", "lw", "sw", "beq", "jalr",
@@ -40,6 +46,93 @@ void main(int argc,char **argv){
    }
    // here you can place your code for the assembler
 
+   // Constructing symbol table
+   symbolTableLen = findSymbolTableLen(assp);
+   pSymbolTable = (struct symbolTable *)malloc(symbolTableLen*sizeof(struct symbolTable));
+   for(i=0; i<symbolTableLen; i++){
+      pSymbolTable[i].symbol = (char *)malloc(7);
+   }
+   noInsts = fillSymbolTable(pSymbolTable, assp);
+
+   // Initializing memory table
+   pMemoryTable = (struct memoryTable *)malloc(memorySize*sizeof(struct memoryTable));
+
+   // for(i=0; i<symbolTableLen; i++){
+   //    printf("sym: %s \t val: %d\n", pSymbolTable[i].symbol, pSymbolTable[i].value);
+   // }
+   // printf("\n\n\n");
+
+   while(getline(&line, &lineSize, assp) != -1){
+      currInst->PC = instCount;
+      instCount++;
+      found = 0;
+      token = strtok(line, "\t, \n");
+      strcpy(lable, token);   // Saving the first word for .fill
+      for(i=0; i<symbolTableLen; i++){
+         // avalin token == sybmolTable[i].symbol, agar yeki bod, miram sare token baadi. token badi 
+         // mishe instruction am. age nabod hamon token instruction man hast.
+         if(strcmp(pSymbolTable[i].symbol, token) == 0){
+            token = strtok(NULL, "\t, \n");
+         }
+      }
+
+      strcpy(currInst->mnemonic, token);
+      currInst->instType = -1;
+
+      if(strcmp(currInst->mnemonic, ".fill") == 0){
+         token = strtok(NULL, "\t, \n");
+         if(isLable(token)){
+            currInst->intInst = getLableValue(pSymbolTable, symbolTableLen, token);
+         }
+         else{
+            currInst->intInst = atoi(token);
+         }
+         printf("currInst->intInst: %d \t token: %s \n", currInst->intInst, token);
+
+         if(memoryCounter < 2048){
+            strcpy(pMemoryTable[memoryCounter].lable, lable);
+            pMemoryTable[memoryCounter].value = currInst->intInst;
+            // printf("%d\n", pMemoryTable[memoryCounter].value);
+            writeToFile(machp, pMemoryTable[memoryCounter].value);
+            memoryCounter++;
+         }
+         else{
+            // *************
+            // Throw exception
+         }
+      }
+
+      if(strcmp(currInst->mnemonic, "lw") == 0){
+         currInst->instType = 1;
+         token = strtok(NULL, "\t, \n");
+         currInst->rt = atoi(token);
+         token = strtok(NULL, "\t, \n");
+         currInst->rs = atoi(token);
+         token = strtok(NULL, "\t, \n");
+         if(isLable(token)){
+            currInst->imm = getLableValue(pSymbolTable, symbolTableLen, token);  
+         }
+         else{
+            currInst->imm = atoi(token);
+         }
+         strcpy(currInst->opCode, "1001");
+         // registers[currInst->rt] = load(registers[currInst->rs] + currInst->imm);
+
+         // printf("rt: %d /t rs: %d \t imm: %d \n", currInst->rt, currInst->rs, currInst->imm);
+      }
+
+      if(strcmp(currInst->mnemonic, "add") == 0){
+         currInst->instType = 0;
+         token = strtok(NULL, "\t, \n");
+         currInst->rd = atoi(token);
+         token = strtok(NULL, "\t, \n");
+         currInst->rs = atoi(token);
+         token = strtok(NULL, "\t, \n");
+         currInst->rt = atoi(token);
+         strcpy(currInst->opCode, "0000");
+         registers[currInst->rd] = registers[currInst->rs] + registers[currInst->rt];
+      }
+   }
 
    fclose(assp);
    fclose(machp);
@@ -84,7 +177,7 @@ int fillSymbolTable(struct symbolTable *symTable, FILE *inputFile){
    return lineNo;
 }
 
-void formatInstruction(struct instruction *, FILE *){
+void formInstruction(struct instruction *, FILE *){
 
 }
 
@@ -128,4 +221,26 @@ void int2Hex16(char *lower, int a){
       lower[1] = lower[0];
       lower[0] = '0';  
    }
+}
+
+int getLableValue(struct symbolTable *symTable,int symTableLen, char *lable){
+   for(int i=0; i<symTableLen; i++){
+      if(strcmp(symTable[i].symbol, lable) == 0){
+         return symTable[i].value;
+      }
+   }
+   // No such a lable
+   return -1;
+}
+
+bool isLable(char *str){
+   int ascii = str[0];
+   if((ascii >= 65 && ascii <= 90) || (ascii >= 97 && ascii <= 122)){
+      return true;
+   }
+   return false;
+}
+
+void writeToFile(FILE *outputFile, int value){
+   fprintf(outputFile, "%d\n", value);
 }
--- assemble.h
+++ assemble.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #define NORTYPE 5
 #define NOITYPE 8
 #define NOJTYPE 2
@@ -15,18 +16,27 @@ struct symbolTable{
 
 struct instruction{
 	size_t instType; 	// 0 means R-type, 1 means I-type, 2 means J-type
-	size_t intInst;
+	size_t intInst;		// Aval instruction ro mibarim be hex va baad tabdil be decimal
 	char *mnemonic;
 	char inst[9];		// Ye reshte ke Hex e insuction hast
+	char opCode[5];	// 4 bit
 	int rs;
 	int rt;
 	int rd;
 	int imm;
-	int PC;				// Instruction count
+	int PC;				// Instruction count YA shomare dast amal
+};
+
+struct memoryTable{
+	char lable[7];
+	int value;
 };
 
 int findSymbolTableLen(FILE *);
 int fillSymbolTable(struct symbolTable *, FILE *);
-void formatInstruction(struct instruction *, FILE *);
+void formInstruction(struct instruction *, FILE *);
 int hex2Int(char *);
 void int2Hex16(char *, int);	// imm value ke 16 bit e
+int getLableValue(struct symbolTable *, int , char *);
+bool isLable(char *);
+void writeToFile(FILE *, int);
